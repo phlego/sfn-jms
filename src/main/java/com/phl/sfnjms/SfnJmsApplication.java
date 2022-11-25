2a4eda42673d3b5e521d8e9f96521f38df56321f
Phuong LE <phuonghoangle@gmail.com>
2022-11-25 13:30:55 +0700

    Setup ActiveMQ Server

--- src/main/java/com/phl/sfnjms/SfnJmsApplication.java
+++ src/main/java/com/phl/sfnjms/SfnJmsApplication.java
@@ -1,13 +1,29 @@
 package com.phl.sfnjms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfnJmsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+		ActiveMQServers
+			.newActiveMQServer(buildActiveMQServerConfig())
+			.start();
+
 		SpringApplication.run(SfnJmsApplication.class, args);
+	}
+
+	private static ConfigurationImpl buildActiveMQServerConfig() throws Exception {
+		return new ConfigurationImpl()
+			.setPersistenceEnabled(false)
+			.setJournalDirectory("target/data/journal")
+			.setSecurityEnabled(false)
+			.addAcceptorConfiguration("invm", "vm://0");
 	}
 
 }
