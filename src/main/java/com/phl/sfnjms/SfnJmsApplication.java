d7499a63970efa86e62453e04aca5e97a7b38348
Phuong LE <phuonghoangle@gmail.com>
2022-11-25 13:16:55 +0700

    Init project

---
+++ src/main/java/com/phl/sfnjms/SfnJmsApplication.java
@@ -0,0 +1,13 @@
+package com.phl.sfnjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfnJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfnJmsApplication.class, args);
+	}
+
+}
